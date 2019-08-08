74c28e0d2a0c348daa5164dfdfe649abf4ab6379
siggwer <jerome.sauvage@etu.unilim.fr>
2019-08-08 22:32:57 +0200

    update trick_list css and refactor picture entity listener

--- src/EntityListener/PictureListener.php
+++ src/EntityListener/PictureListener.php
@@ -3,6 +3,7 @@
 namespace App\EntityListener;
 
 use App\Entity\Picture;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 /**
  * Class TrickListener
@@ -38,6 +39,37 @@ class PictureListener
         $picture->getUploadedFile()->move($this->uploadDir, $filename);
         $picture->setPath("uploads/".$filename);
 
+    }
+
+    /**
+     * @param Picture $picture
+     *
+     * @param PreUpdateEventArgs $eventArgs
+     */
+    public function preUpdate(Picture $picture, PreUpdateEventArgs $eventArgs)
+    {
+       if ($eventArgs->getOldValue("path") !== null && $picture->getUploadedFile() !== null) {
+           unlink($this->uploadDir . '/' . $picture->getPath());
+       }
+       $this->upload($picture);
+
+    }
+
+    /**
+     * @param Picture $picture
+     */
+    private function upload(Picture $picture)
+    {
+        if ($picture->getUploadedFile() === null) {
+            return;
+        }
+            $filename = md5(uniqid("", true)). "." . $picture
+                    ->getUploadedFile()
+                    ->getClientOriginalExtension();
+
+            $picture->getUploadedFile()->move($this->uploadDir, $filename);
+            $picture->setPath("uploads/".$filename);
+        
     }
 
     /**
